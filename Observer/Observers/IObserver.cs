ec4327be476cd49e883a0a694c76eb8bc62beaeb
bawser <66109423+dabawser@users.noreply.github.com>
2026-02-13 22:28:01 +0100

    Add interfaces (#10)
    Co-authored-by: bawser <git.attendant524@passmail.com>

---
+++ Observer/Observers/IObserver.cs
@@ -0,0 +1,18 @@
+﻿namespace Observer.Observers;
+
+/// <summary>
+/// The IObserver interface defines the contract for an observer in the Observer design pattern. It requires
+/// implementing classes to define the Update method, which will be called by the subject to notify the observer of
+/// changes in the subject's state.
+/// </summary>
+public interface IObserver
+{
+    /// <summary>
+    /// The Update method is called by the subject to notify the observer of changes in the subject's state. When this
+    /// method is called, the observer can react to the change in the subject's state, such as by updating its own
+    /// state or performing some action based on the new state of the subject. This method is a key part of the
+    /// Observer design pattern, as it allows for a decoupled way of notifying observers about changes in the subject's
+    /// state without the subject needing to know the details of the observers.
+    /// </summary>
+    void Update();
+}
---
+++ Observer/Subjects/ISubject.cs
@@ -0,0 +1,33 @@
+﻿using Observer.Observers;
+
+namespace Observer.Subjects;
+
+/// <summary>
+/// The ISubject interface defines the contract for a subject in the Observer design pattern. It allows observers to
+/// register, remove, and be notified of changes in the subject's state.
+/// </summary>
+public interface ISubject
+{
+    /// <summary>
+    /// Registers an observer to receive updates from the subject. When the subject's state changes, it will notify all
+    /// registered observers by calling their Update method. This allows observers to react to changes in the subject's
+    /// state.
+    /// </summary>
+    void RegisterObserver(IObserver observer);
+
+    /// <summary>
+    /// Removes an observer from the list of registered observers. After an observer is removed, it will no longer
+    /// receive updates from the subject when the subject's state changes. This allows observers to stop receiving
+    /// notifications when they are no longer interested in the subject's state changes.
+    /// </summary>
+    void RemoveObserver(IObserver observer);
+
+    /// <summary>
+    /// Notifies all registered observers of a change in the subject's state. When this method is called, the subject
+    /// will iterate through its list of registered observers and call their Update method, allowing them to react
+    /// accordingly to the change in the subject's state. This is a key part of the Observer design pattern, as it
+    /// allows for a decoupled way of notifying observers about changes in the subject's state without the subject
+    /// needing to know the details of the observers.
+    /// </summary>
+    void NotifyObservers();
+}
